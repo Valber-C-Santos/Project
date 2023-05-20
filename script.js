fd8e12cc34d0cb22e6dd55505e85adb1a10082ae
Valber Candido <valber_558@hotmail.com>
2023-05-20 14:00:45 -0300

    Requisito-12

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const divCores = document.getElementsByClassName('color');
 const sessionEscolha = document.getElementById('pixel-board');
 const sessaoRestaura = document.querySelector('#botao-restaura');
 const btnrestaura = document.createElement('button');
+const salvaCores = document.querySelectorAll('pixelBoard');
 
 function geradorCor() {
   let letras = '0123456789abcdef'
@@ -52,22 +53,20 @@ function tresQuadrado() {
     localStorage.setItem('colorPalette', JSON.stringify(paleta));
   }
 }
-function botao(random) {
 
+function botao(random) {
   btn.setAttribute('id', 'button-random-color');
   btn.innerText = 'Cores aleatórias';
   paletasession.appendChild(btn);
 }
 
 function botaoRestore() {
-
   btnrestaura.setAttribute('id', 'clear-board');
   btnrestaura.innerText = 'Limpar';
   sessaoRestaura.appendChild(btnrestaura);
 }
 
 function cincoXCinco() {
-
   for (let index = 0; index < 25; index += 1) {
     const createQuadrado = document.createElement('div');
     createQuadrado.style.backgroundColor = 'white';
@@ -85,14 +84,48 @@ function selecaoCor() {
     })
   }
 }
+
 const paintPixel = () => {
   const guardPixel = document.querySelectorAll('.pixel');
   for (let index = 0; index < guardPixel.length; index += 1) {
     guardPixel[index].addEventListener('click', function (event) {
       const selectedColor = document.querySelector('.selected').style.backgroundColor;
       event.target.style.backgroundColor = selectedColor;
+
+      const savedPixels = JSON.parse(localStorage.getItem('pixelBoard')) || [];
+      const pixelData = {
+        position: index,
+        color: selectedColor
+      };
+      savedPixels.push(pixelData);
+      localStorage.setItem('pixelBoard', JSON.stringify(savedPixels));
     });
   }
+}
+
+function restoreDrawing() {
+  const savedPixels = JSON.parse(localStorage.getItem('pixelBoard')) || [];
+
+  savedPixels.forEach((pixel) => {
+    const pixelElement = document.querySelector(`.pixel:nth-child(${pixel.position + 1})`);
+    pixelElement.style.backgroundColor = pixel.color;
+  });
+}
+
+function saveDrawing() {
+  const savedPixels = [];
+  const guardPixel = document.querySelectorAll('.pixel');
+
+  guardPixel.forEach((pixel, index) => {
+    const color = pixel.style.backgroundColor;
+    const pixelData = {
+      position: index,
+      color: color
+    };
+    savedPixels.push(pixelData);
+  });
+
+  localStorage.setItem('pixelBoard', JSON.stringify(savedPixels));
 }
 
 cincoXCinco();
@@ -101,26 +134,27 @@ botao();
 selecaoCor();
 paintPixel();
 botaoRestore();
+restoreDrawing();
 
 btn.addEventListener('click', () => {
-
   for (let index = 1; index < divCores.length; index += 1) {
     let mistura = divCores[index];
     mistura.style.backgroundColor = geradorCor();
   }
   const paleta = [];
-    const divs = document.querySelectorAll('.color');
-    divs.forEach((div) => {
-        paleta.push(div.style.backgroundColor);
-    });
-    localStorage.setItem('colorPalette', JSON.stringify(paleta));
+  const divs = document.querySelectorAll('.color');
+  divs.forEach((div) => {
+    paleta.push(div.style.backgroundColor);
+  });
+  localStorage.setItem('colorPalette', JSON.stringify(paleta));
 });
 
 btnrestaura.addEventListener('click', () => {
-
   const guarPixel = document.querySelectorAll('.pixel');
   for (let index = 0; index < guarPixel.length; index += 1) {
     let apaga = guarPixel[index];
     apaga.style.backgroundColor = 'white';
   }
 });
+
+window.addEventListener('beforeunload', saveDrawing);
